c77f66f02f11966d160116bff7f8097f4cfc3b9e
Incerteza88 <info@incerteza.es>
2025-04-15 16:30:13 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  const sujeto = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  const verbo = ['se comío', ' se bebió', 'aplastó', 'rompió'];
+  const objeto = ['mis deberes', 'mi teléfono', 'el coche'];
+  const momento = ['después de clase', 'cuando yo estaba durmiendo', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras rezaba'];
+  
+function generarExcusa() {
+  
+  let sujetoRandom = sujeto[Math.floor(Math.random() *sujeto.length)];
+  let verboRandom = verbo[Math.floor(Math.random() *verbo.length)];
+  let objetoRandom = objeto[Math.floor(Math.random() *objeto.length)];
+  let momentoRandom = momento[Math.floor(Math.random() *momento.length)];
+  
+  let frase= (sujetoRandom  + " " + verboRandom + " " + objetoRandom + " " + momentoRandom);
+
+  document.querySelector('#excusa').innerHTML = frase;
+
+  }
+  
+generarExcusa();
+
+}
